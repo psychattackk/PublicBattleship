2ad2e7ed15604771cd706b1c2cab3ff286da41dd
psychattackk <sheldonmcclish@gmail.com>
2024-06-27 22:30:13 -0500

    Update battleship.java

--- battleship.java
+++ battleship.java
@@ -1,11 +1,11 @@
 /*Main file to run the game.  
  * This will call the actiongame.java file to run.*/
 
-public class SheldonMcClish_battleship
+public class psychattackk_battleship
 {
 	public static void main(String[] args)
 	{
-		SheldonMcClish_actiongame game = new SheldonMcClish_actiongame();
+		psychattackk_actiongame game = new psychattackk_actiongame();
 		game.runGame(); // Run game from actiongame. 
 	}
 }// end class battleship.java
